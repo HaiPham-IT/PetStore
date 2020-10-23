6ed796054540f42b981ed9d47a1b989165f000e1
pdhai <pdhai6696@gmail.com>
2020-10-22 17:25:17 -0700

    update

--- server/src/service/product-service.js
+++ server/src/service/product-service.js
@@ -1,10 +1,24 @@
 const Product = require('../model/product-schema')
+const productvalidation = require('../validation/product-validation')
+const Validator = require('validatorjs')
+const productErrMess = require('../validation/errors-message/product-validation-errors-message')
 
 const createProduct = (req, res) => {
     let {proId, proName, trademark, proType, description, rate} = req.body
 
-    console.log(req.body)
-    console.log(proName)
+    let product = {
+        proId: proId,
+        proName: proName,
+        trademark: trademark,
+        proType: proType,
+        description: description,
+        rate: rate
+    }
+
+    let validator = new Validator(product, productvalidation, productErrMess)
+
+    
+
 }
 
 module.exports = {createProduct}
--- server/src/validation/errors-message/product-validation-errors-message.js
+++ server/src/validation/errors-message/product-validation-errors-message.js
@@ -1,3 +1,9 @@
 module.exports = {
-   
+    required: ':attribute is missing',
+    min: {
+        string: 'Rate is between 0 to 5'
+    },
+    max: {
+        string: 'Rate is between 0 to 5'
+    }
 }
